c9763e20f86efada8a33d001264c9d551e23a2ee
phil <pri@irbe.ch>
2021-07-22 13:35:25 +0200

    add form save and load function

--- internal/server/static/app.js
+++ internal/server/static/app.js
@@ -16,8 +16,83 @@ if (!token)
    prettyPrint: function(obj) {
       var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
       return JSON.stringify(obj, null, 3)
-         .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-         .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+         .replace(/&/g, '&').replace(/\\"/g, '"')
+         .replace(/</g, '<').replace(/>/g, '>')
          .replace(jsonLine, token.json.replacer);
       }
    };
+
+var Form = Form || {
+   data: {},
+   Save: function(el = document.querySelectorAll("input"), key = "Form") {
+      el.forEach(e => {
+         if(e.name != null || e.name != ""){
+            let d = this.data[e.name] = {type: e.nodeName};
+            switch(e.nodeName){
+               case "INPUT":{
+                  switch(e.type.toLowerCase()){
+                     case "checkbox":{
+                        d.value = e.checked;
+                        break;
+                     }
+                     case "radio":{
+                        if(e.checked){d.value = e.value};
+                        break;
+                     }
+                     default:{
+                        d.value = e.value;
+                        break;
+                     }
+                  }
+                  break;
+               }
+               case "SELECT":{
+                  d.value = e.selectedIndex;
+                  break;
+               }
+            }
+         }
+      })
+      localStorage.setItem(key, JSON.stringify(this.data));
+   },
+
+   Load: function(el = document.querySelectorAll("input"), key = "Form") {
+      let d = JSON.parse(localStorage.getItem(key));
+      if(d === null){return;}
+      for(e of el){
+         if(e.name in d){
+            switch(e.nodeName){
+               case "INPUT":{
+                  //handle checkboxes via click trigger to fire attached events
+                  switch(e.type.toLowerCase()){
+                     case "checkbox": {
+                        if(e.checked !== d[e.name].value){
+                           e.click();
+                        }
+                        break;
+                     }
+                     case "radio":{
+                        el.forEach(r => {if(r.value == d[e.name].value){r.checked=true;}})
+                        break;
+                     }
+                     default: {
+                        e.value = d[e.name].value;
+                        break;
+                     }
+                  }
+                  break;
+               }
+               case "SELECT":{
+                  e.selectedIndex = d[e.name].value;
+                  // for materializecss we have to init the element again
+                  if(M !== null){
+                     M.FormSelect.init(e, {});
+                  }
+                  break;
+               }
+            }
+         }
+
+      }
+   }
+}
